73648b45d55cbb2f2650b595136b980df5b31d64
bavv <bavuvanet@gmail.com>
2017-12-06 00:04:34 +0700

    custom style renderer dialog

--- android/src/main/java/org/droidupnp/view/RendererDialog.java
+++ android/src/main/java/org/droidupnp/view/RendererDialog.java
@@ -13,62 +13,59 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
 import android.widget.ArrayAdapter;
 
 import com.reactlibrary.R;
 
 public class RendererDialog extends DialogFragment {
 
-	private Callable<Void> callback = null;
+    private Callable<Void> callback = null;
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState)
-	{
-		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(
+                new ContextThemeWrapper(getActivity(), R.style.AlertDialogCustom)
+        );
 
-		final Collection<IUpnpDevice> upnpDevices = Main.upnpServiceController.getServiceListener()
-				.getFilteredDeviceList(new CallableRendererFilter());
+        final Collection<IUpnpDevice> upnpDevices = Main.upnpServiceController.getServiceListener()
+                .getFilteredDeviceList(new CallableRendererFilter());
 
-		ArrayList<DeviceDisplay> list = new ArrayList<DeviceDisplay>();
-		for (IUpnpDevice upnpDevice : upnpDevices)
-			list.add(new DeviceDisplay(upnpDevice));
+        ArrayList<DeviceDisplay> list = new ArrayList<DeviceDisplay>();
+        for (IUpnpDevice upnpDevice : upnpDevices)
+            list.add(new DeviceDisplay(upnpDevice));
 
-		final DialogFragment dialog = this;
+        final DialogFragment dialog = this;
 
-		if(list.size()==0)
-		{
-			builder.setTitle(R.string.selectRenderer)
-				.setMessage(R.string.noRenderer)
-				.setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which)
-					{
-						dialog.dismiss();
-					}
-				});
-		}
-		else
-		{
-			ArrayAdapter<DeviceDisplay> rendererList = new ArrayAdapter<DeviceDisplay>(getActivity(),
-				android.R.layout.simple_list_item_1, list);
-			builder.setTitle(R.string.selectRenderer).setAdapter(rendererList, new DialogInterface.OnClickListener() {
-				@Override
-				public void onClick(DialogInterface dialog, int which) {
-					Main.upnpServiceController.setSelectedRenderer((IUpnpDevice) upnpDevices.toArray()[which]);
-					try {
-						if (callback != null)
-							callback.call();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			});
-		}
-		return builder.create();
-	}
+        if (list.size() == 0) {
+            builder.setTitle(R.string.selectRenderer)
+                    .setMessage(R.string.noRenderer)
+                    .setPositiveButton(R.string.OK, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+        } else {
+            ArrayAdapter<DeviceDisplay> rendererList = new ArrayAdapter<DeviceDisplay>(getActivity(),
+                    R.layout.renderer_list_item, R.id.rendererName, list);
+            builder.setTitle(R.string.selectRenderer).setAdapter(rendererList, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Main.upnpServiceController.setSelectedRenderer((IUpnpDevice) upnpDevices.toArray()[which]);
+                    try {
+                        if (callback != null)
+                            callback.call();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        return builder.create();
+    }
 
-	public void setCallback(Callable<Void> callback)
-	{
-		this.callback = callback;
-	}
+    public void setCallback(Callable<Void> callback) {
+        this.callback = callback;
+    }
 }
